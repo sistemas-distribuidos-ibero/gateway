9dbfbaa3453c2235596875f2643fe335bf1cb5d2
EmilioRivera0 <elcholula619@gmail.com>
2024-05-07 10:38:23 -0600

    Cart service endpoints

--- main.py
+++ main.py
@@ -64,6 +64,28 @@ def delete_order(order_id):
     response = delete(os.environ['ORDERS_SERVICE']+f'/orders/{order_id}')
     return jsonify(response.json()), response.status_code
 
+# cart endpoints --------
+@app.route('/cart', methods=['POST'])
+def add_to_cart():
+    data = request.json
+    # consume the service
+    response = post(os.environ['CART_SERVICE']+'/cart', json=data)
+    return jsonify(response.json()), response.status_code
+
+@app.route('/cart', methods=['GET'])
+def get_cart():
+    data = request.json
+    # consume the service
+    response = get(os.environ['CART_SERVICE']+'/cart', json=data)
+    return jsonify(response.json()), response.status_code
+
+@app.route('/cart', methods=['DELETE'])
+def del_cart():
+    data = request.json
+    # consume the service
+    response = delete(os.environ['CART_SERVICE']+'/cart', json=data)
+    return jsonify(response.json()), response.status_code
+
 # authentication endpoints --------
 
 
